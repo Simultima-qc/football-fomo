ef06b320a6c4df424c2ab05d8b9a2aee5ae8e683
Nicolas Pelletier <nicolas.pelletier@neo6.ca>
2026-04-21 11:50:49 -0400

    Fix GA link typing

--- src/components/shared/GaLink.tsx
+++ src/components/shared/GaLink.tsx
@@ -2,15 +2,24 @@
 import Link from "next/link";
 import type { ComponentProps } from "react";
 
+type GtagEventParams = Record<string, string | number>;
+type Gtag = (command: "event", eventName: string, params: GtagEventParams) => void;
+
+declare global {
+  interface Window {
+    gtag?: Gtag;
+  }
+}
+
 type Props = ComponentProps<typeof Link> & {
   gaEvent: string;
-  gaParams?: Record<string, string | number>;
+  gaParams?: GtagEventParams;
 };
 
 export function GaLink({ gaEvent, gaParams, onClick, href, ...props }: Props) {
   function track() {
-    if (typeof window !== "undefined" && "gtag" in window) {
-      (window as any).gtag("event", gaEvent, gaParams ?? {});
+    if (typeof window !== "undefined" && window.gtag) {
+      window.gtag("event", gaEvent, gaParams ?? {});
     }
   }
 
